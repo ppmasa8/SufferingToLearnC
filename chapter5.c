e372a94ddc06dacb307a9e4e63000ca8f8e5a697
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-01 00:11:39 +0900

    add calc

---
+++ chapter5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(void) {
+    int x,y,cash,regester;
+    double tax;
+
+    x, y = 198, 138*2;
+    cash = 1000;
+    tax  = 1.1;
+    regester = cash - (x + y) * tax;
+    printf("%d円\n", regester);
+
+}
